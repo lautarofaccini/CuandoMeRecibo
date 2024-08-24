77ae0894b7391c2dc93fe2e90807f4d9b31fad61
Lautaro Faccini <lautarofaccini@gmail.com>
2024-08-23 22:15:29 -0300

    Cambios en pos a la relacion Tiene y el form

--- src/app/dashboard/mis-materias/CheckboxTiene.jsx
+++ src/app/dashboard/mis-materias/CheckboxTiene.jsx
@@ -2,16 +2,45 @@ import { Checkbox } from "@nextui-org/react";
 import { useState } from "react";
 
 function CheckboxTiene({ materia }) {
-  const [isSelected, setIsSelected] = useState(false);
+  const [isSelectedReg, setIsSelectedReg] = useState(false);
+  const [isSelectedApr, setIsSelectedApr] = useState(false);
+
+  function toggleSelection(box) {
+    if (box === 0) {
+      if (isSelectedReg) {
+        setIsSelectedReg(false);
+      } else {
+        setIsSelectedReg(true);
+        setIsSelectedApr(false);
+      }
+    } else if (box === 1) {
+      if (isSelectedApr) {
+        setIsSelectedApr(false);
+      } else {
+        setIsSelectedApr(true);
+        setIsSelectedReg(false);
+      }
+    }
+  }
 
   return (
     <div className="grid grid-cols-2 gap-4">
       <div className="flex justify-center">
-            <Checkbox value={materia.id} />
-          </div>
-          <div className="flex justify-center">
-            <Checkbox value={materia.id} />
-          </div>
+        <Checkbox
+          isSelected={isSelectedReg}
+          value={materia.id}
+          name={"regularizada"}
+          onValueChange={() => toggleSelection(0)}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Checkbox
+          isSelected={isSelectedApr}
+          value={materia.id}
+          name={"aprobada"}
+          onValueChange={() => toggleSelection(1)}
+        />
+      </div>
     </div>
   );
 }
--- src/app/dashboard/mis-materias/page.jsx
+++ src/app/dashboard/mis-materias/page.jsx
@@ -1,19 +1,104 @@
 "use client";
 import { Button } from "@nextui-org/react";
 import FormTiene from "./FormTiene";
+
+/* async function ActualizarCondiciones(nombreCondicion, condicion, data, id) {
+  async function updateRegular(nuevoId, viejoId) {
+    const data = {
+      idAnteces: nuevoId,
+    };
+    const res = await axios.put(
+      "/api/condicion?cond=" +
+        nombreCondicion +
+        "&id1=" +
+        viejoId +
+        "&id2=" +
+        id,
+      data
+    );
+    return res.data;
+  }
+
+  async function createRegular(nuevoId) {
+    const data = {
+      idAnteces: nuevoId,
+      idSuces: id,
+    };
+    const res = await axios.post(
+      "/api/condicion?cond=" + nombreCondicion,
+      data
+    );
+    return res.data;
+  }
+
+  async function deleteRegular(idElim) {
+    const res = await axios.delete(
+      "/api/condicion?cond=" + nombreCondicion + "&id1=" + idElim + "&id2=" + id
+    );
+    if (res.status !== 204) {
+      console.log("Error al eliminar materia: ", res);
+    }
+  }
+
+  if (JSON.stringify(data) !== JSON.stringify(condicion)) {
+    //! Si en algun momento se desordenan las listas, esta comparacion podria dar un falso positivo
+    const dataAux = data.filter((elemento) => !condicion.includes(elemento));
+    const condAux = condicion.filter((elemento) => !data.includes(elemento));
+    const Iguales = condicion.filter((elemento) => data.includes(elemento));
+    let salida = [];
+    for (let i = 0; i < Iguales.length; i++) {
+      salida.push({
+        idAnteces: Iguales[i],
+        idSuces: id,
+      });
+    }
+    if (dataAux.length !== condAux.length) {
+      if (dataAux.length > condAux.length) {
+        let dLenght = dataAux.length;
+        for (let i = 0; i < dLenght - condAux.length; i++) {
+          salida.push(createRegular(dataAux[0]));
+          dataAux.shift();
+        }
+      } else {
+        let rLenght = condAux.length;
+        for (let i = 0; i < rLenght - dataAux.length; i++) {
+          deleteRegular(condAux[0]);
+          condAux.shift();
+        }
+      }
+    }
+    if (dataAux.length !== 0 || condAux.length !== 0) {
+      // Misma longitud
+      for (let i = 0; i < condAux.length; i++) {
+        salida.push(updateRegular(dataAux[0], condAux[i]));
+        dataAux.shift();
+      }
+    }
+    // Esperar a que todas las promesas se resuelvan
+    salida = await Promise.all(salida);
+    return salida.map((reg) => reg.idAnteces).sort((a, b) => a - b);
+    //TODO: Testear con mas convinaciones
+  } else {
+    return condicion;
+  }
+} */
+
 function MisMateriasPage() {
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     console.log(formData);
-    /* const dataReg = formData
+    const dataReg = formData
       .getAll("regularizada")
       .map((item) => parseInt(item, 10));
     const dataAp = formData
       .getAll("aprobada")
-      .map((item) => parseInt(item, 10)); 
-
-    try {
+      .map((item) => parseInt(item, 10));
+      //TODO: Adaptar esto a este caso
+      //TODO: Enviar a la base de datos las materias para este alumno
+      //TODO: Tiene q mandar el dni y el id de la materia para vincular a los dos 
+    /* try {
       const [regResult, apResult] = await Promise.all([
         ActualizarCondiciones("regularizada", regularizada, dataReg, paramId),
         ActualizarCondiciones("aprobada", aprobada, dataAp, paramId),
@@ -26,8 +111,7 @@ function MisMateriasPage() {
     } catch (error) {
       console.error("Error al actualizar correlatividades:", error);
       router.push("/materias");
-    }
-      */
+    } */
   };
 
   return (
